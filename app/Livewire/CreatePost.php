264ae3addd693b010aea6a6efbd0861f1f5dfa26
LukaD45 <luka.dusko44@gmail.com>
2025-03-29 20:02:03 +0100

    validate form data using Rule attribute

--- app/Livewire/CreatePost.php
+++ app/Livewire/CreatePost.php
@@ -4,15 +4,21 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Post;
+use Livewire\Attributes\Rule;
 
 class CreatePost extends Component
 {
 
+    #[Rule("required")]
     public $title;
+
+    #[Rule("required")]
     public $content;
 
     public function save()
     {
+        $this->validate();
+
         Post::create([
             'title' => $this->title,
             'content' => $this->content,
--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -4,12 +4,14 @@
     <label>
         <span>Title</span>
         <input type="text" wire:model="title">
+        @error('title') <em>{{ $message }}</em> @enderror
     </label>
 
 
     <label>
         <span>Content</span>
         <textarea wire:model="content"></textarea>
+        @error('content') <em>{{ $message }}</em> @enderror
     </label>
 
     <button type="submit">Save</button>
