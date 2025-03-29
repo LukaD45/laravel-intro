e76f69dbb2a0bfecb429b56f5527c96b108f2e30
LukaD45 <luka.dusko44@gmail.com>
2025-03-29 20:31:39 +0100

    count characters in content textarea

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -1,5 +1,11 @@
 <div>
 <h2>New Post</h2>
+
+<div x-data="{count:0}">
+    <span x-text="count"></span>
+
+    <button type="button" x-on:click="$wire.save()">submit</button>
+</div>
 <form wire:submit="save">
     <label>
         <span>Title</span>
@@ -11,6 +17,7 @@
     <label>
         <span>Content</span>
         <textarea wire:model="content"></textarea>
+        <small>Characters: <span x-text="$wire.content.length"></span></small>
         @error('content') <em>{{ $message }}</em> @enderror
     </label>
 
